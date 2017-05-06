0f612cc821956b405b2d1a833273cd4e6298ce0e
Rob Thijssen <rthijssen@gmail.com>
2017-05-06 15:48:59 +0300

    yo angular boilerplate

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name steelhorseadventuresApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the steelhorseadventuresApp
+ */
+angular.module('steelhorseadventuresApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name steelhorseadventuresApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the steelhorseadventuresApp
+ */
+angular.module('steelhorseadventuresApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
