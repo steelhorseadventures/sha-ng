730817504471c7f2158696637b936346d5695a4b
Rob Thijssen <rthijssen@gmail.com>
2017-05-09 14:08:09 +0300

    pics after maps

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,17 +10,28 @@
 angular.module('steelhorseadventuresApp')
   .controller('MainCtrl', function ($scope, thingApi) {
     $scope.things = [];
+
     thingApi.query({org: 'steelhorseadventures', repo: 'sha-content', file: 'things.json'}, function (thingList) {
       for (var i in thingList) {
         if (!(thingList[i].hide) && !!(thingList[i].id)) {
           $scope.things.push({
             title: thingList[i].title,
+            header_picture: thingList[i].header_picture,
             countries: thingList[i].countries,
             pictures: thingList[i].pictures,
             waypoints: thingList[i].waypoints,
             summary: thingList[i].description,
             map: thingList[i].map,
-            facebook: thingList[i].facebook
+            facebook: thingList[i].facebook,
+            duration: [
+              'roughly ' + thingList[i].map.duration.text,
+              thingList[i].map.duration.text + ' or something like that',
+              thingList[i].map.duration.text + ', honestly, officer',
+              thingList[i].map.duration.text + ', or thereabouts',
+              'maybe ' + thingList[i].map.duration.text,
+              'close enough to ' + thingList[i].map.duration.text,
+              'something like ' + thingList[i].map.duration.text,
+            ][Math.floor(Math.random()*7)]
           });
         }
       }
